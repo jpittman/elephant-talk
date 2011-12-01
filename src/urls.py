b9411b2f69ab582f44560305bc5b897717a9cefe
Johnnie Pittman <jpittman@group6.net>
2011-12-01 10:20:34 -0800

    Add in the auth urls we'll use for handling things like user auth and password management.
    Also adding in a robot text handler for safe keeping.
    
    Oh and admin.

--- src/urls.py
+++ src/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -14,4 +14,20 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    
+    ## auth
+    (r'^login/$', 'django.contrib.auth.views.login'),    
+    (r'^logout/$', 'django.contrib.auth.views.logout_then_login'),
+    (r'^change_password/$', 'django.contrib.auth.views.password_change', 
+        {'template_name': 'registration/change_password.html', 
+        'post_change_redirect':'/password_done/'}),
+    (r'^password_done/$', 'django.contrib.auth.views.password_change_done',
+        {'template_name': 'registration/password_success.html'}),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+    
+    # handling web crawlers.
+    (r'^robots\.txt$', 
+        lambda r: HttpResponse("User-agent: *\nDisallow: /", mimetype="text/plain")),
 )
