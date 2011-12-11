34ce863a4d1bf78898d1854c7994a6b770091129
Johnnie Pittman <jpittman@group6.net>
2011-12-11 15:57:04 -0800

    Adding in pagination

--- src/posts/views.py
+++ src/posts/views.py
@@ -4,10 +4,12 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, get_list_or_404
 from django.shortcuts import render_to_response, redirect
 from django.template import Context, RequestContext
+
 
 from pdb import set_trace as st
 
@@ -27,6 +29,19 @@ def not_staff_or_superuser(user):
 def index(request):
     # return all base classes and order them by name.
     posts = Post.objects.all()
+    paginator = Paginator(posts, 5)
+    
+    # Make sure page request is an int. If not, deliver first page.
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+    
+    # If page request (9999) is out of range, deliver last page of results.
+    try:
+        posts = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        posts = paginator.page(paginator.num_pages)
     
     template = 'index.html'
     return render_to_response(
