429cd0b49ba2133bff050c4c83d521c0d2d4de71
Johnnie Pittman <jpittman@group6.net>
2011-12-11 10:43:38 -0800

    Adding a related name to the user foreign key.  This allows me to do user.posts to get the posts by a particular user.

--- src/posts/models.py
+++ src/posts/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
 
     user_help = "The user the post is associated with."
-    user = models.ForeignKey(User, help_text=user_help)
+    user = models.ForeignKey(User, related_name="posts", help_text=user_help)
 
     title_help = "The title of the post."
     title = models.CharField(max_length=256, help_text=title_help)
